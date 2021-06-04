dd38704a2899b80eae90b7d73e86f83dd8f9d05b
Daniel Vaughan <dbvaughan@gmail.com>
2021-06-03 17:27:31 -0700

    Add project files.

---
+++ ReferencingAssembliesLocator/Locator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReferencingAssembliesLocator
+{
+	public class Locator
+	{
+		public async Task<Dictionary<string, HashSet<string>>> GetReferencedAssembliesAsync(string assemblyNamePattern, string directory, IProgress<double> progress = default, CancellationToken cancelationToken = default)
+		{
+			var references = await GetReferencedAssembliesAsync(directory, progress, cancelationToken);
+			return GetReferencedAssemblies(assemblyNamePattern, references);
+		}
+
+		public Dictionary<string, HashSet<string>> GetReferencedAssemblies(string assemblyNamePattern, Dictionary<string, HashSet<string>> references)
+		{
+			Regex regex = new Regex(assemblyNamePattern, RegexOptions.Compiled);
+
+			Dictionary<string, HashSet<string>> result = new Dictionary<string, HashSet<string>>();
+
+			foreach (KeyValuePair<string, HashSet<string>> item in references)
+			{
+				HashSet<string> referenceCollection = item.Value;
+
+				foreach (string reference in referenceCollection)
+				{
+					if (regex.IsMatch(reference))
+					{
+						string referencingAssembly = item.Key;
+						if (!result.TryGetValue(referencingAssembly, out var hashSet))
+						{
+							hashSet = new HashSet<string>();
+							result[referencingAssembly] = hashSet;
+						}
+						hashSet.Add(reference);
+					}
+				}
+			}
+
+			return result;
+		}
+
+		public Task<Dictionary<string, HashSet<string>>> GetReferencedAssembliesAsync(string directory, IProgress<double> progress = default, CancellationToken cancelationToken = default)
+		{
+			ConcurrentDictionary<string, ConcurrentBag<string>> dictionary = new ConcurrentDictionary<string, ConcurrentBag<string>>();
+
+			List<string> dllPaths = Directory.EnumerateFiles(directory, "*.dll", SearchOption.AllDirectories).ToList();
+			
+			int filesToProgress = dllPaths.Count;
+			int counter = 0;
+
+			Parallel.ForEach(dllPaths, 
+							new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount }, 
+							(dllPath, state) =>
+			{
+				cancelationToken.ThrowIfCancellationRequested();
+
+				try
+				{
+					Assembly assembly = Assembly.LoadFrom(dllPath);
+					AssemblyName[] referencedAssemblies = assembly.GetReferencedAssemblies();
+					string ownerName = assembly.FullName + $" ({dllPath})";
+
+					foreach (AssemblyName referenceName in referencedAssemblies)
+					{
+						string fullName = referenceName.FullName;
+
+						var bag = dictionary.GetOrAdd(ownerName, _ => new ConcurrentBag<string>());
+						bag.Add(fullName);
+					}
+				}
+				catch (Exception)
+				{
+					// ignore and continue;
+				}
+
+				if (progress != null)
+				{
+					Interlocked.Increment(ref counter);
+					progress.Report(counter / (double)filesToProgress);
+				}
+			});
+
+			Dictionary<string, HashSet<string>> result = new Dictionary<string, HashSet<string>>();
+
+			foreach (KeyValuePair<string, ConcurrentBag<string>> pair in dictionary)
+			{
+				result.Add(pair.Key, pair.Value.ToHashSet<string>());
+			}
+
+			return Task.FromResult(result);
+		}
+	}
+}
---
+++ ReferencingAssembliesLocator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReferencingAssembliesLocator
+{
+	class Program
+	{
+		async static Task Main(string[] args)
+		{
+			int argsLength = args.Length;
+			string directory;
+
+			if (argsLength > 0)
+			{
+				directory = args[0];
+			}
+			else
+			{
+				directory = Directory.GetCurrentDirectory();
+			}
+
+			var locator = new Locator();
+
+			Dictionary<string, HashSet<string>> references;
+
+			using (var progress = new ProgressBar())
+			{
+				references = await locator.GetReferencedAssembliesAsync(directory, progress);
+			}
+
+			if (!references.Any())
+			{
+				Console.WriteLine("No results.");
+				Console.ReadKey();
+				goto quit;
+			}
+
+			void OutputResults(Dictionary<string, HashSet<string>> result)
+			{
+				Console.ResetColor();
+				Console.WriteLine();
+
+				foreach (KeyValuePair<string, HashSet<string>> pair in result)
+				{
+					string referencingAssembly = pair.Key;
+					Console.ForegroundColor = ConsoleColor.White;
+					Console.BackgroundColor = ConsoleColor.DarkYellow;
+					Console.WriteLine(referencingAssembly);
+					Console.ResetColor();
+					Console.WriteLine("  Has reference to:");
+					foreach (string reference in pair.Value)
+					{
+						Console.WriteLine("  " + reference);
+					}
+					
+					Console.WriteLine();
+				}
+			}
+
+			while (true)
+			{
+				Console.WriteLine("Enter assembly name pattern or 'q' to quit.");
+				string assemblyPattern = Console.ReadLine();
+				if (assemblyPattern.Trim().Equals("q", StringComparison.OrdinalIgnoreCase))
+				{
+					break;
+				}
+
+				var items = locator.GetReferencedAssemblies(assemblyPattern, references);
+				OutputResults(items);
+			}
+
+			quit:;
+		}
+
+	}
+}
